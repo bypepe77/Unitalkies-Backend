16e682b9c033536b5b2865bdade64dd72298a024
Mario Espada <mario.espada77@gmail.com>
2019-11-05 19:35:38 +0100

    ADD university name in model user

--- models/User.js
+++ models/User.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 const UserSchema = new Schema(
-    {
-        username: {type: String},
-        hashedPassword: {type: String}
-    },
-    {
-        timestamps: {
-          createdAt: 'created_at',
-          updatedAt: 'updated_at',
-        },
-      },
-
-
+  {
+    username: { type: String },
+    hashedPassword: { type: String }
+  },
+  {
+    university_name: { type: String }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
 );
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
