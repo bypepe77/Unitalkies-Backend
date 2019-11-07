bc360795b3c5ff9a6a2d4ffd209f3229d9f185fa
Mario Espada <mario.espada77@gmail.com>
2019-11-07 19:38:30 +0100

    ADD populate university in posts

--- routes/profile.js
+++ routes/profile.js
@@ -5,16 +5,18 @@ const Post = require("../models/Post");
 const User = require("../models/User");
 
 router.get("/:username", async (req, res, next) => {
-    const { username } = req.params;
-    console.log(username);
-    try {
-      const userProfile = await User.find({username})
-      const posts = await Post.find({username: userProfile[0]._id}).populate("username").sort('-created_at')
-      res.json({userProfile, posts});
-    } catch (error) {
-      next(error);
-    }
-  });
-
+  const { username } = req.params;
+  console.log(username);
+  try {
+    const userProfile = await User.find({ username });
+    const posts = await Post.find({ username: userProfile[0]._id })
+      .populate("username")
+      .populate("formUni")
+      .sort("-created_at");
+    res.json({ userProfile, posts });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
