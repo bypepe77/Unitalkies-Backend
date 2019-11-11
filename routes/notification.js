f94108326f9084097eee9fe600f498c9fbfaf78f
Mario Espada <mario.espada77@gmail.com>
2019-11-11 04:54:51 +0100

    List all notifications

--- routes/notification.js
+++ routes/notification.js
@@ -3,9 +3,21 @@ const router = express.Router();
 
 const Post = require("../models/Post");
 const User = require("../models/User");
+const Notification = require("../models/Notifications");
 
 router.get("/all", async (req, res, next) => {
-  console.log("Notifications");
+  const username = req.session.currentUser._id;
+
+  try {
+      const notificationCount = await Notification.find({
+        notificationTo: username,
+        visited: 0,
+      });
+      res.json(notificationCount);
+      
+  } catch (error) {
+      console.log(error);
+  }
 
 });
 
