290e0c466f63dd0db2c9f123efec4c185c543ca2
Mario Espada <mario.espada77@gmail.com>
2019-11-12 19:15:32 +0100

    Deploy to master

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
-const cors = require("cors");
+const cors = require("cors")({ origin: true, credentials: true });
 require("dotenv").config();
 
 mongoose.set("useCreateIndex", true);
@@ -29,6 +28,10 @@ const followRouter = require("./routes/follows");
 const notificationRouter = require("./routes/notification");
 var app = express();
 
+app.set("trust proxy", true);
+app.use(cors);
+app.options("*", cors);
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
@@ -36,7 +39,6 @@ app.set("view engine", "hbs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
@@ -48,18 +50,15 @@ app.use(
     secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true,
+    name: "unitalkies-ih", // configuracion del nombre de la cookie
     cookie: {
-      maxAge: 24 * 60 * 60 * 1000
+      maxAge: 24 * 60 * 60 * 1000,
+      sameSite: "none", // esta es la linea importante
+      secure: process.env.NODE_ENV === "production"
     }
   })
 );
 
-app.use(
-  cors({
-    credentials: true,
-    origin: [process.env.FRONTEND_URL]
-  })
-);
 app.use((req, res, next) => {
   app.locals.currentUser = req.session.currentUser;
   next();
